f8b8559f1274f9f71fdf664940123ce691800bb5
SL8620 <2584032673@qq.com>
2026-03-02 08:23:44 +0800

    455完成

--- 455.分发饼干.cpp
+++ 455.分发饼干.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author: SL8620
  * @Date: 2026-02-10 19:30:19
- * @LastEditTime: 2026-02-10 19:31:29
+ * @LastEditTime: 2026-03-02 08:22:55
  * @LastEditors: SL8620
  * @Description: 
  * @FilePath: \.leetcode\455.分发饼干.cpp
@@ -19,7 +19,36 @@ class Solution
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) 
     {
-        
+        int ans=0;
+        // 对饼干大小和饥饿值进行排序
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        if(g.empty() || s.empty())
+        {
+            return 0;
+        }
+
+        if(g.front()>s.back())
+        {
+            return 0;
+        }
+
+        int biscuit_index = 0;
+        for(int i=0; i<g.size(); i++)
+        {
+            for(int j=biscuit_index; j<s.size(); j++)
+            {
+                if(g[i]<=s[j])
+                {
+                    biscuit_index = j+1;
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+
     }
 };
 // @lc code=end
