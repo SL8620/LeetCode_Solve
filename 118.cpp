72200779d84b2542d220144e79004706d40eb155
SL8620 <2584032673@qq.com>
2026-09-08 08:15:26 +0800

    update

---
+++ 118.cpp
@@ -0,0 +1,52 @@
+/*
+118. 杨辉三角
+
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+ 
+
+示例 1:
+输入: numRows = 5
+输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+示例 2:
+输入: numRows = 1
+输出: [[1]]
+ 
+
+提示:  1 <= numRows <= 30
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>> ret;
+        vector<int> first = {1};
+        vector<int> second = {1,1};
+        ret.push_back(first);
+        ret.push_back(second);
+
+        if(numRows<=2)
+        {
+            return (numRows==1)?vector<vector<int>>{first}:ret; 
+        }
+        else
+        {
+            for(int i=2; i<numRows; i++)
+            {
+                vector<int> ins(i+1, 1);
+                for(int j=1; j<i; j++)
+                {
+                    ins[j] = ret[i-1][j-1] + ret[i-1][j];
+                }
+                ret.push_back(ins);
+            }
+        }
+        return ret;
+    }
+};
